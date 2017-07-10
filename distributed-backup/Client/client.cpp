d4c112819936dd1ab127f1893e2469e0828558ea
Andrea Perez Quintana <alu0100843101@ull.edu.es>
2017-07-10 03:36:09 +0100

    Cliente con algunos fallos

--- distributed-backup/Client/client.cpp
+++ distributed-backup/Client/client.cpp
@@ -6,9 +6,63 @@ Client::Client(QWidget *parent) :
     ui(new Ui::Client)
 {
     ui->setupUi(this);
+
+    //El cliente puede enviar o recibir datos, si envía los envía siempre al servidor LocalHost.
+    //ui->ConnectPushButton->setEnabled(false);
+    ui->SendPushButton->setEnabled(false);
+    ui->DestinatioAddressLineEdit->setEnabled(false);
 }
 
 Client::~Client()
 {
     delete ui;
+}
+
+void Client::on_ConnectPushButton_clicked()
+{
+    destination_ = new SendDestination(filename_,
+                                       files_,
+                                       QHostAddress (ui->ServerAddressLineEdit->text()),
+                                       ui->ServerPortLineEdit->text().toInt());
+    destination_->run();
+}
+
+void Client::on_SendPushButton_clicked()
+{
+    ui->SendPushButton->setEnabled(false);
+    server_ = new MyQTcpServer(QHostAddress(ui->DestinatioAddressLineEdit->text()),
+                               ui->DestinationPortLineEdit->text().toInt(),
+                               ui->DestinationFolderLineEdit->text(),
+                               this);
+    server_->start();
+}
+
+void Client::on_OriginFindPushButton_clicked()
+{
+    filename_ = QFileDialog::getExistingDirectory(this, "Open", "/home", QFileDialog::ShowDirsOnly);
+
+    if (filename_ != "") {
+        ui->OriginFolderLineEdit->setText(filename_);
+
+        QDirIterator it(filename_, QStringList() << "-", QDir::Files, QDirIterator::Subdirectories);
+        while (it.hasNext()){
+            files_.push_back(it.next());
+        }
+        ui->ConnectPushButton->setEnabled(true);
+    }
+    else {
+        qDebug() << "No se puede abrir la carpeta";
+    }
+}
+
+void Client::on_DestinationFindPushButton_clicked()
+{
+    filenameD_ = QFileDialog::getExistingDirectory(this, "Open", "/home", QFileDialog::ShowDirsOnly);
+    if (filenameD_ != "") {
+        ui->DestinationFolderLineEdit->setText(filenameD_);
+        ui->SendPushButton->setEnabled(true);
+    }
+    else {
+        qDebug() << "No se puede abrir la carpeta";
+    }
 }
--- distributed-backup/Client/client.h
+++ distributed-backup/Client/client.h
@@ -3,6 +3,9 @@
 
 #include <QMainWindow>
 
+#include "myqtcpserver.h"
+#include "senddestination.h"
+
 namespace Ui {
 class Client;
 }
@@ -15,8 +18,20 @@ public:
     explicit Client(QWidget *parent = 0);
     ~Client();
 
+private slots:
+    void on_ConnectPushButton_clicked();
+    void on_SendPushButton_clicked();
+    void on_OriginFindPushButton_clicked();
+    void on_DestinationFindPushButton_clicked();
+
 private:
     Ui::Client *ui;
+
+    MyQTcpServer *server_;
+    SendDestination *destination_;
+
+    QString filename_, filenameD_;
+    QStringList files_;
 };
 
 #endif // CLIENT_H
